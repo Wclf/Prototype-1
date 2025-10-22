c752127ccacfae8ab90c1a133b10020fb4956c14
wclf <thang10102k3@gmail.com>
2025-10-23 01:05:15 +0700

    switch2camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class CameraSwitch : MonoBehaviour
 {
-    [SerializeField] private Camera firstPersonCamera;
-    [SerializeField] private Camera thirdPersonCamera;
-
-    private bool isThirdCameraView = true;
+    public Camera[] cameras; // Array to hold the cameras
+    private int currentCameraIndex;
 
     void Start()
     {
-        ApplyView(); // đặt trạng thái ban đầu
+        // Disable all cameras except the first one (MainCamera)
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            cameras[i].gameObject.SetActive(i == 0);
+        }
+        currentCameraIndex = 0; // Start with the MainCamera
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.C))
         {
-            isThirdCameraView = !isThirdCameraView;
-            ApplyView(); // áp dụng ngay khi bấm E
+            SwitchCamera();
         }
     }
 
-    private void ApplyView()
+    public void SwitchCamera()
     {
-        firstPersonCamera.gameObject.SetActive(!isThirdCameraView);
-        thirdPersonCamera.gameObject.SetActive(isThirdCameraView);
-    }
+        // Disable the current camera
+        cameras[currentCameraIndex].gameObject.SetActive(false);
 
+        // Update the index to the next camera
+        currentCameraIndex = (currentCameraIndex + 1) % cameras.Length;
+
+        // Enable the new current camera
+        cameras[currentCameraIndex].gameObject.SetActive(true);
+    }
 }
